294a9af1fc65109c9858533b2c5ca43b3f200b75
github username <github email address>
2019-02-18 14:35:03 -0500

    add header and notebookinput components files

---
+++ src/components/NotebookInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class NotebookInput extends React.Component {
+    render(){
+        return(
+            <div>
+                <form>
+                    
+                </form>
+            </div>
+        )
+    }
+}
