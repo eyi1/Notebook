5638944f64f22f6f559900b90bb70de5ed55d278
github username <github email address>
2019-02-19 13:11:41 -0500

    add change and submit handlers for input and form

--- src/components/NotebookInput.js
+++ src/components/NotebookInput.js
@@ -1,13 +1,42 @@
 import React from 'react'
 
 export default class NotebookInput extends React.Component {
+    constructor(){
+        super()
+        this.state={
+            name: '',
+        }
+    }
+
+    handleOnChange = event => {     
+        const { value, name } = event.target;
+        this.setState({
+          [name]: value
+        })
+    }
+
+    // handleOnChange(e){
+    //     this.setState=({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
+
+    handleOnSubmit = event => {
+        event.preventDefault();
+        
+    }
+
     render(){
         return(
             <div>
-                <form>
-                    <input></input>
+                <form className='notebook-form' onSubmit={this.handleOnSubmit} >
+                    {/* <label htmlFor="notebookName" className="col-md-4 control-label">Name:</label> */}
+                    <label htmlFor="notebookName">name:</label>
+                    <input type="text" name='name' onChange={this.handleOnChange} value={this.state.name} placeholder="notebook name"/><br />
+                    <button>save notebook</button>
                 </form>
-            </div>
+                {this.state.name}
+            </div>           
         )
     }
 }
