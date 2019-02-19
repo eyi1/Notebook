0a195a866e15b8f82825fe5c48a6b5d0e2a47607
github username <github email address>
2019-02-19 15:26:25 -0500

    import compose from redux

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
-//import { Provider } from 'react-redux';
-//import { createStore, applyMiddleware} from 'redux';
-//import thunk from 'redux-thunk';
-//import rootReducer from './reducers/index';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 
 //const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(
-//<Provider store={store}>
-    <App />,
-//</Provider>, 
+<Provider store={store}>
+    <App />
+</Provider>, 
 document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
