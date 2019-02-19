f36fcf3de598593ca5593c5ede7d3df9c4a0aa59
github username <github email address>
2019-02-19 13:12:01 -0500

    add button and click handler

--- src/containers/NotebooksContainer.js
+++ src/containers/NotebooksContainer.js
@@ -1,12 +1,43 @@
 import React from 'react'
 import NotebookInput from '../components/NotebookInput'
+//import Popup from "reactjs-popup";
 
-export default class NoteBooksContainer extends React.Component {
+export default class NoteBooksContainer extends React.Component {  
+    constructor(){
+        super();
+        this.state = {
+            render: ''
+        }
+    }
+     
+    handleClick(component, e){
+        console.log(component);
+        this.setState({
+            render: component
+        });        
+    }
+    
+    _renderSubComponent(){
+        // switch(this.state.render){
+        //     case 'notebookInput': return <NotebookInput />
+        //     default:
+        //     return ''
+        //     }
+        if (this.state.render === 'notebookInput'){
+            return <NotebookInput />
+        }
+    }
+   
     render(){
         return(
             <div>
                 {/* <NotebookSideBar /> */}
-                <NotebookInput />
+                <div>
+                    <button onClick={this.handleClick.bind(this, 'notebookInput')}> + new notebook</button>
+                </div>
+                <div>
+                    {this._renderSubComponent()}
+                </div>
             </div>
         )        
     }
