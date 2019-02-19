5b8247f1042d408a563a18610d5410c01a1adfb0
github username <github email address>
2019-02-19 15:30:00 -0500

    add notebooks: notebooksReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import notebooksReducer from './notebooksReducer'
+
 
 const rootReducer = combineReducers({ 
-
+notebooks: notebooksReducer
 })
 
 export default rootReducer;
