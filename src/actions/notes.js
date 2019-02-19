abe92d57dea553998f46fa83ae4a8af243f333c2
github username <github email address>
2019-02-18 22:45:10 -0500

    add actions file

---
+++ src/actions/notes.js
@@ -0,0 +1,40 @@
+const notebooksLink = `/api/v1/notes`
+
+//action creators
+// const collectNotes = notes => {
+//     return{
+//         type: 'REQUEST_NOTES',
+//         notes
+//     }
+// }
+
+// const addNote = note => {
+//     return{
+//         type: 'ADD_NOTE',
+//         note
+//     }
+// }
+
+// //updates form
+// const updateNote = note => {
+//     return{
+//         type: 'UPDATE_NOTE',
+//         note
+//     } 
+// }
+
+// const removeNote = note => {
+//     return{
+//         type: 'DELETE_NOTE',
+//         note
+//     }
+// }
+
+// Async Actions
+
+export const getNotes = () => {
+    return (dispatch) => {
+        return fetch(notebooksLink)
+            .then(response => console.log(response.json()))
+    }
+}
