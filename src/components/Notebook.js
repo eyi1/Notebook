6b99b3318c8242c09abfecc634f4d6b6e2f3b7f6
github username <github email address>
2019-02-19 20:06:30 -0500

    create and render table with Notebook attributes

---
+++ src/components/Notebook.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Notebook extends React.Component {
+    render(){
+        return(
+            <div>{this.props.notebook}</div>
+        )
+    }
+}
+
+export default Notebook
