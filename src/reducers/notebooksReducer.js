772e9f46145f499474cde88bd99591ccedea47e2
github username <github email address>
2019-02-20 17:10:22 -0500

    delete notebook, update state and return new notebooks array

--- src/reducers/notebooksReducer.js
+++ src/reducers/notebooksReducer.js
@@ -3,16 +3,21 @@ import * as types from '../actions/actionTypes';
 export default (state=[], action) => {
     switch(action.type){
         case types.REQUEST_NOTEBOOKS:
-        console.log(action.notebooks)
+        //console.log(action.notebooks)
             return action.notebooks
-        
-        default:
-        return state
 
         case types.ADD_NOTEBOOK:
-            console.log(action.notebook)
+           // console.log(action.notebook)
+            console.log(action)
             //return state.concat(action.notebook)
             return [...state, action.notebook]
         
+        case types.DELETE_NOTEBOOK:
+        //     console.log(state)
+            const notebooks = state.filter(notebook => notebook.id !== action.id)
+            return notebooks
+
+        default:
+        return state       
     }
 }
