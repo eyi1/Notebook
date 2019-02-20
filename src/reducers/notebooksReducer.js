2e51860078f0832f713dc6284238c4714a79a6ac
github username <github email address>
2019-02-19 20:11:13 -0500

    add new case types.ADD_NOTEBOOK

--- src/reducers/notebooksReducer.js
+++ src/reducers/notebooksReducer.js
@@ -2,11 +2,16 @@ import * as types from '../actions/actionTypes';
 
 export default (state=[], action) => {
     switch(action.type){
-        case types. REQUEST_NOTEBOOKS:
+        case types.REQUEST_NOTEBOOKS:
+        console.log(action.notebooks)
             return action.notebooks
         
         default:
         return state
+
+        case types.ADD_NOTEBOOK:
+            console.log(action.notebook)
+
         
     }
 }
