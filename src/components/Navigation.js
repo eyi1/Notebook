37216da17f3c6bc5108ecf926a3743c4a439e524
github username <github email address>
2019-02-20 18:42:14 -0500

    update actions in dropdown

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,11 +13,11 @@ export default class Navigation extends React.Component {
                         <Nav.Link href="#link">sign up</Nav.Link>
                         <Nav.Link href="#link">login</Nav.Link>
                         <NavDropdown title="dropdown" id="basic-nav-dropdown" right>
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">My Account</NavDropdown.Item>
+                             <NavDropdown.Item href="#action/3.2">All Notebooks</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">All Notes</NavDropdown.Item>
                             <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">Sign Out</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
     {/* <Form inline>
