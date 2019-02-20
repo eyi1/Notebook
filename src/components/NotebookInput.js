c3134ccc49d69789a73c2a64ff59fe1a3f129af7
github username <github email address>
2019-02-19 20:07:23 -0500

    add Modal and componentWillReceiveProps(nextProps)

--- src/components/NotebookInput.js
+++ src/components/NotebookInput.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
-export default class NotebookInput extends React.Component {
-    constructor(){
-        super()
+import Modal from 'react-bootstrap/Modal'
+import { Button } from 'react-bootstrap';
+import { createNotebook } from '../actions/notebooks'
+import { connect } from 'react-redux'
+ 
+class NotebookInput extends React.Component {
+    constructor(props){
+        super(props)
         this.state={
+            id: '',
             name: '',
+            show: props.modal
         }
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(this.state.show!==nextProps.modal){ //false vs true
+        this.setState({show: nextProps.modal})
+      }
     }
 
     handleOnChange = event => {     
@@ -17,21 +29,41 @@ export default class NotebookInput extends React.Component {
 
     handleOnSubmit = event => {
         event.preventDefault();
-
-        
+        this.props.createNotebook(this.state.name)
+        //debugger;
     }
 
     render(){
+
+        let close = () => this.setState({ show: false});
+
         return(
-            <div>
-                <form className='notebook-form' onSubmit={this.handleOnSubmit} >
-                    {/* <label htmlFor="notebookName" className="col-md-4 control-label">Name:</label> */}
-                    <label htmlFor="notebookName">name:</label>
-                    <input type="text" name='name' onChange={this.handleOnChange} value={this.state.name} placeholder="notebook name"/><br />
-                    <button>save notebook</button>
-                </form>
-                {this.state.name}
-            </div>           
+        <div>      
+            <div className="modal-container">
+            <Modal
+              show={this.state.show}
+              onHide={close}
+              container={this}
+              aria-labelledby="contained-modal-title"
+            >
+              <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title">Notebook</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                     <form className="form-group" onSubmit={this.handleOnSubmit}>
+                         <label>name:</label>
+                         <input type="text" className="form-control" name='name' onChange={this.handleOnChange} value={this.state.name} placeholder="notebook name"/><br />
+                         <Button bsStyle="primary" type="submit">Save</Button>
+                     </form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={close}>Close</Button>
+              </Modal.Footer>
+            </Modal>
+          </div>    
+        </div>
         )
     }
 }
+
+export default connect(null, {createNotebook})(NotebookInput)
