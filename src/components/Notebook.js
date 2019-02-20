5385c189e468028477aca3a65c54d22a26657573
github username <github email address>
2019-02-20 17:08:08 -0500

    use container row and col to render notebook attributes and add dropdown for edit and delete actions

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,13 +1,54 @@
 import React from 'react'
+import { Dropdown} from 'react-bootstrap';
+import {Container, Row, Col }from 'react-bootstrap'
+
 
 const Notebook = (props) =>
-    <div className="row">
-        <div className="col-md-4">
-            {props.notebook.id}
+        <div>
+            <Container>
+                <Row>
+                    <Col sm>{props.notebook.id}</Col>
+                    <Col sm>{props.notebook.name}</Col>
+                    <Col sm>2 of 2</Col>
+                    <Col sm>2 of 2</Col>
+                    <Col sm>
+                        <Dropdown>
+                            <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
+                            </Dropdown.Toggle>
+                            
+                            <Dropdown.Menu>            
+                                <Dropdown.Item as="button">Edit</Dropdown.Item>
+                                <Dropdown.Item as="button">Delete</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Col>
+                </Row>
+            </Container>
         </div>
-        <div className="col-md-6">
-            {props.notebook.name}
-        </div>
-    </div>
 
 export default Notebook
+
+            // <div className="container">
+    // <div className="row">
+    //     <div className="col-sm-1">
+    //         {props.notebook.id}
+    //     </div>
+    //     <div className="col-lg-5">
+    //         {props.notebook.name}
+    //     </div>
+    //     <div className="col-lg-2">1</div>
+    //     <div className="col-lg-2">2</div>
+    //     <div className="col-lg-2">
+        //   <Dropdown>
+        //      <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
+        //      </Dropdown.Toggle>
+             
+        //      <Dropdown.Menu>            
+        //         <Dropdown.Item as="button">Edit</Dropdown.Item>
+        //         <Dropdown.Item as="button">Delete</Dropdown.Item>
+        //     </Dropdown.Menu>
+        //  </Dropdown>
+    //     </div>
+    //     <br />
+    // </div>
+    // </div>
