08d8613befcf6203c6b238d44c9216a012a989cc
github username <github email address>
2019-02-21 11:41:55 -0500

    import form from boostrap-react and add input fields

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Form } from 'react-bootstrap'
 
 class NoteInput extends React.Component{
     constructor(){
         super()
         this.state={
-            id='',
-            title='',
-            content='',
+            id:'',
+            title: '',
+            content:'',
         }
     }
     render(){
         return(
             <div>
-                
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlInput1">
+                        <Form.Label>Title</Form.Label>
+                        <Form.Control type="text" placeholder="Title" />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>Write Something Here</Form.Label>
+                        <Form.Control as="textarea" rows="5" />
+                    </Form.Group>
+                    </Form>
             </div>
         )
     }
