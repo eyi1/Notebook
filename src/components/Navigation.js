084dd908791f25c6f4513dbbc8e6f5aaec3d6597
github username <github email address>
2019-02-21 09:06:07 -0500

    change one of nav dropdown item to route to /notebooks path

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home"><strong>noteböök</strong></Navbar.Brand>
+                <Navbar.Brand href="#"><strong>noteböök</strong></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
@@ -13,12 +13,13 @@ export default class Navigation extends React.Component {
                         <Nav.Link href="/signup">sign up</Nav.Link>
                         <Nav.Link href="/login">login</Nav.Link>
                         
-                        <NavDropdown title="dropdown" id="basic-nav-dropdown" right>
-                            <NavDropdown.Item href="#action/3.1">My Account</NavDropdown.Item>
-                             <NavDropdown.Item href="#action/3.2">All Notebooks</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">All Notes</NavDropdown.Item>
+                        
+                        <NavDropdown title="more" id="basic-nav-dropdown" right>
+                            <NavDropdown.Item href="#action/3.1">my account</NavDropdown.Item>
+                             <NavDropdown.Item href="/notebooks">all notebooks</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">all notes</NavDropdown.Item>
                             <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Sign Out</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.4">sign out</NavDropdown.Item>
                         </NavDropdown>
 
                     </Nav>
