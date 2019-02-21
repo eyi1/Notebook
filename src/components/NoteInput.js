a20743620572da36722809f107cc8985db4820e1
github username <github email address>
2019-02-21 11:48:51 -0500

    add onSubmit event handler and submit button disabled feature if there is no input

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -10,10 +10,23 @@ class NoteInput extends React.Component{
             content:'',
         }
     }
+
+    handleOnChange = event => {
+        const { value, name } = event.target;
+            this.setState({
+                [name]: value
+        })
+    }
+
+    handOnSubmit = event => {
+        event.preventDefault();
+    }
+
     render(){
+        const isEnabled = this.state.title.length && this.state.content.length > 0;
         return(
             <div>
-                <Form>
+                <Form onSubmit={this.handOnSubmit}>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Title</Form.Label>
                         <Form.Control type="text" name='title' onChange={this.handleOnChange} value={this.state.title} placeholder="Title" />
@@ -22,6 +35,7 @@ class NoteInput extends React.Component{
                         <Form.Label>Write Something Here</Form.Label>
                         <Form.Control as="textarea" name='content' onChange={this.handleOnChange} value={this.state.content} rows="10" />
                     </Form.Group>
+                    <Button disabled={!isEnabled} bsStyle="primary" type="submit">Save</Button>
                     </Form>
             </div>
         )
