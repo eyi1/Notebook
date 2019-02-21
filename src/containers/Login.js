01f9df4847134fb87cd1907daa78b5f87af3a55b
github username <github email address>
2019-02-21 10:43:57 -0500

    add col sizing of md-4

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -14,7 +14,7 @@ class Login extends React.Component {
         return(
             <div>
                 <Form className="login-form" onSubmit="handleOnSubmit">
-                    <Form.Group as={Col} controlId="formGridEmail">
+                    <Form.Group as={Col} md="4" controlId="formGridEmail">
                         <Form.Label>Email</Form.Label>
                         <Form.Control name="email" onChange={this.handleOnChange} type="email" placeholder="Enter email" value={this.state.email} />
                         <Form.Text className="text-muted">
@@ -22,11 +22,11 @@ class Login extends React.Component {
                         </Form.Text>
                     </Form.Group>
 
-                    <Form.Group as={Col} controlId="formGridPassword">
+                    <Form.Group as={Col} md="4" controlId="formGridPassword">
                         <Form.Label>Password</Form.Label>
                         <Form.Control name="password" onChange={this.handleOnChange }type="password" placeholder="Password" value={this.state.password} />
                     </Form.Group>
-                    <Button className="signup-btn" variant="primary" type="submit">
+                    <Button className="login-btn" variant="primary" type="submit">
                         Login
                     </Button>
                 </Form>
