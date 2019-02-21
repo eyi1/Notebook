d2acc50f87d224bca33df9ea8af80bd5758cdf53
github username <github email address>
2019-02-21 11:06:08 -0500

    create constructor and set initial state

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
 class NoteInput extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            id='',
+            title='',
+            content='',
+        }
+    }
     render(){
         return(
             <div>
-
+                
             </div>
         )
     }
