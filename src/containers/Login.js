2f1e039ffa9b02560e995ba15fd2051ca90561ca
github username <github email address>
2019-02-21 00:12:27 -0500

    add form features for user login

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -3,7 +3,25 @@ import React from 'react'
 class Login extends React.Component {
     render(){
         return(
-            <div></div>
+            <div>
+                <Form className="login-form" onSubmit="handleOnSubmit">
+                    <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control name="email" onChange={this.handleOnChange} type="email" placeholder="Enter email" value={this.state.email} />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control name="password" onChange={this.handleOnChange }type="password" placeholder="Password" value={this.state.password} />
+                    </Form.Group>
+                    <Button className="signup-btn" variant="primary" type="submit">
+                        Login
+                    </Button>
+                </Form>
+            </div>
         )
     }
 }
