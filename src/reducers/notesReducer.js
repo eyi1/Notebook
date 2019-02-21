c98d4443bdb111d0c3c661295dc9514e5555c1cc
github username <github email address>
2019-02-21 14:03:42 -0500

    handle update request notes

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -2,8 +2,12 @@ import * as types from '../actions/actionTypes';
 
 export default (state=[], action) => {
     switch(action.type){
+        
         case types.REQUEST_NOTES:
-
+        console.log('action.notes:', action.notes)
         return action.notes
+
+        default:
+        return state
     }
 }
