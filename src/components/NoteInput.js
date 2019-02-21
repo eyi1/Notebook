7905d3b24f32a4da27ab9e6ad4577b94fff39942
github username <github email address>
2019-02-21 11:45:57 -0500

    include onChange event handler to inputs

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -16,11 +16,11 @@ class NoteInput extends React.Component{
                 <Form>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Title</Form.Label>
-                        <Form.Control type="text" placeholder="Title" />
+                        <Form.Control type="text" name='title' onChange={this.handleOnChange} value={this.state.title} placeholder="Title" />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Write Something Here</Form.Label>
-                        <Form.Control as="textarea" rows="5" />
+                        <Form.Control as="textarea" name='content' onChange={this.handleOnChange} value={this.state.content} rows="10" />
                     </Form.Group>
                     </Form>
             </div>
