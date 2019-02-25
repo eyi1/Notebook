6df9b409189fb1f4cf8f643242332d847aad0107
github username <github email address>
2019-02-25 18:52:39 -0500

    pass props to router link component - line 14

--- src/components/Notebook.js
+++ src/components/Notebook.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Dropdown} from 'react-bootstrap';
 import {Container, Row, Col, Button }from 'react-bootstrap'
+//import NotesContainer from '../containers/NotesContainer';
+import { Link } from 'react-router-dom'
 
 
 const Notebook = (props) =>
@@ -8,7 +10,9 @@ const Notebook = (props) =>
             <Container>
                 <Row>
                     <Col sm="2">{props.notebook.id}</Col>
-                    <Col sm="4"><Button href="/notes" variant="link">{props.notebook.name}</Button></Col>
+                    {/* <Col sm="4"><Button onClick={()=> <NotesContainer notebook={props.notebook}/>}>{props.notebook.name}</Button></Col> */}
+                    <Col><Link to={{pathname: '/notebooks/:id/notes', state:{ notebookId: props.notebook.id}}}>{props.notebook.name}</Link></Col>   
+                    {/* <Col><Link to={{pathname: '/notes', state:{ notebookId: props.notebook.id}}}>{props.notebook.name}</Link></Col> */}
                     <Col sm="2">2 of 2</Col>
                     <Col sm="2">2 of 2</Col>
                     <Col sm="2">
@@ -26,29 +30,58 @@ const Notebook = (props) =>
             </Container>
         </div>
 
-export default Notebook
+//class Notebook extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state={
+//             props: props,
+//             render: ''
+//         }
+//     }
 
-            // <div className="container">
-    // <div className="row">
-    //     <div className="col-sm-1">
-    //         {props.notebook.id}
-    //     </div>
-    //     <div className="col-lg-5">
-    //         {props.notebook.name}
-    //     </div>
-    //     <div className="col-lg-2">1</div>
-    //     <div className="col-lg-2">2</div>
-    //     <div className="col-lg-2">
-        //   <Dropdown>
-        //      <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
-        //      </Dropdown.Toggle>
-             
-        //      <Dropdown.Menu>            
-        //         <Dropdown.Item as="button">Edit</Dropdown.Item>
-        //         <Dropdown.Item as="button">Delete</Dropdown.Item>
-        //     </Dropdown.Menu>
-        //  </Dropdown>
-    //     </div>
-    //     <br />
-    // </div>
-    // </div>
+//     handleClick(compName, e){
+//         console.log(compName);
+//         this.setState({render: compName});        
+//     }
+
+//     renderSubComp() {
+//         if (this.state.render === 'notesContainer'){
+//             return <NotesContainer notebook={this.state.props.notebook} />
+//         }
+//     }
+
+//     render(){
+//         return(
+//             <div>
+//             <Container>
+//                 <Row>
+//                     <Col sm="2">{this.state.props.notebook.id}</Col>
+//                     <Col sm="4"><Button variant="light" onClick={this.handleClick.bind(this, 'notesContainer')}>{this.state.props.notebook.name}</Button></Col>
+//                     {/* <Col sm="4"><button onClick={()=> <NotesContainer notebookId={this.state.props.notebook.id}/>}>{this.state.props.notebook.name}</button></Col> */}
+//                     {/* <Col sm="4"><Button href='/notes'>{this.state.props.notebook.name}</Button></Col> */}
+//                     {/* href='/notes'  */}
+//                     <Col sm="2">EJ</Col>
+//                     <Col sm="2">2/20/19</Col>
+//                     <Col sm="2">
+//                         <Dropdown>
+//                             <Dropdown.Toggle variant="Secondary" id="dropdown-basic">
+//                             </Dropdown.Toggle>
+                            
+//                             <Dropdown.Menu>            
+//                                 <Dropdown.Item as="button">Edit</Dropdown.Item>
+//                                 <Dropdown.Item as="button" onClick={() => this.state.props.deleteNotebook(this.state.props.notebook.id)}>Delete</Dropdown.Item>
+//                             </Dropdown.Menu>
+//                         </Dropdown>
+//                     </Col>
+//                 </Row>
+//             </Container>
+//             <div>
+//             {this.renderSubComp()}
+//             </div>
+//             </div>
+//         )
+//     }
+// }
+
+
+export default Notebook
