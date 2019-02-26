5a454c865062b32b4e284e5223efbcb6b2f21288
github username <github email address>
2019-02-25 19:32:00 -0500

    comment out constructor

--- src/containers/NotesContainer.js
+++ src/containers/NotesContainer.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import NoteInput from '../components/NoteInput'
+import Notes from '../components/Notes'
 import { getNotes } from '../actions/noteActions'
 import { connect } from 'react-redux';
 
 class NotesContainer extends React.Component{
+    // constructor(props){
+    //     super(props)
+    //     this.state={
+    //         notebookId: props.notebook.id
+    //     }
+    // }
 
     componentDidMount(){
-        this.props.getNotes()
+        //const { id } = this.props.match.params
+        
+        console.log(this.props.location.state)
+        console.log(this.props.location)
+        const id = JSON.stringify(this.props.location.state.notebookId)
+        console.log(id)
+        console.log(typeof id)
+        
+        // const id = JSON.stringify(this.state.notebookId)
+        this.props.getNotes(id)
     }
 
     render(){
+        // const renderNotes = this.props.notesCollection.map(note => note.title)
         return(
             <div>
-                <NoteInput />
+                {/* <NoteInput /> */}
+                <Notes notesCollection={this.props.notesCollection}/>
             </div>
         )
     }
 }
 
-// const mapStateToProps = state => {
+const mapStateToProps = state => {
+    return{
+        notesCollection: state.notes
+    }
+}
 
-// }
 
-
-export default connect(null, {getNotes})(NotesContainer)
+export default connect(mapStateToProps, {getNotes})(NotesContainer)
