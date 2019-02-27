5ee948bdd6697c090ef20d1bfc47413fc302c1a8
github username <github email address>
2019-02-26 21:58:13 -0500

    import card from react-bootstrap and render note props title, id, and content

---
+++ src/components/Note.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+class Note extends React.Component {
+    render(){
+        return(
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>{this.props.note.title}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">id: {this.props.note.id}</Card.Subtitle>
+                        <Card.Text>
+                            {this.props.note.content}
+                        </Card.Text>
+                        <Card.Link href="#">Edit</Card.Link>
+                        <Card.Link href="#">Delete</Card.Link>
+                    </Card.Body>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default Note
