2b170ce3ae0af966ed6b0bf1e6b6ab6565720ce5
github username <github email address>
2019-02-26 21:07:41 -0500

    build createNote action with a method of post

--- src/actions/noteActions.js
+++ src/actions/noteActions.js
@@ -1,6 +1,7 @@
 import * as types from './actionTypes'
-const notesLink = `/api/v1/notebooks`
-const generalNotes = `/api/v1/notes`
+import { ModalTitle } from 'react-bootstrap';
+const notebooksLink = `/api/v1/notebooks`
+//const notesLink = `/api/v1/notes`
 
 //action creators
 const setNotes = notes => {
@@ -12,12 +13,13 @@ const setNotes = notes => {
     }
 }
 
-// const addNote = note => {
-//     return{
-//         type: 'ADD_NOTE',
-//         note
-//     }
-// }
+const addNote = note => {
+    console.log(note)
+    return{
+        type: 'ADD_NOTE',
+        note
+    }
+}
 
 // //updates form
 // const updateNote = note => {
@@ -40,14 +42,39 @@ export const getNotes = (notebookId) => {
     //debugger;
     console.log(typeof notebookId)
     return (dispatch) => {
-        fetch(`${notesLink}/${notebookId}/notes`)
-            .then(response => response.json())
-            .then(notes => {
+        fetch(`${notebooksLink}/${notebookId}/notes`)
+        .then(response => response.json())
+        .then(notes => {
                 dispatch(setNotes(notes))
-            })
-            .catch(error => console.log(error))
+        })
+        .catch(error => console.log(error))
     }
 }
+
+export const createNote = (note) => {
+    console.log(note)
+    const notebookId = note.notebookId
+    const title = note.title 
+    const content = note.content
+
+    return (dispatch) => {
+        fetch(`${notebooksLink}/${notebookId}/notes`, {
+            method: 'POST',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({title: title, content: content, notebookId: notebookId})
+        })
+        //.then(response => console.log(response))
+        .then(response => response.json())
+        //.then(note => console.log(note))
+        .then(note => {
+            dispatch(addNote(note))
+             //debugger;
+        })
+        .catch(error => console.log(error))
+   }
+ }
 
 // export const getNotes = () => {
 //     debugger;
