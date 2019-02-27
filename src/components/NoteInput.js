b91fab4069e1e545149a30c8d6bec7ec80af0636
github username <github email address>
2019-02-26 20:06:46 -0500

    change each col grid size to 12

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -9,6 +9,7 @@ class NoteInput extends React.Component{
             id:'',
             title: '',
             content:'',
+            //notebookId:
         }
     }
 
@@ -30,17 +31,17 @@ class NoteInput extends React.Component{
                 <Form onSubmit={this.handOnSubmit}>
                     <Form.Group controlId="exampleForm.ControlInput1">
                         {/* <Form.Label>Title</Form.Label> */}
-                        <Col sm="8" >
+                        <Col sm="12" >
                             <Form.Control size="lg" type="text" name='title' onChange={this.handleOnChange} value={this.state.title} placeholder="Title" />
                         </Col>
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         {/* <Form.Label>Write Something Here</Form.Label> */}
-                        <Col sm="8">
+                        <Col sm="12">
                             <Form.Control as="textarea" name='content' onChange={this.handleOnChange} value={this.state.content} rows="10" placeholder="Start writing"/>
                         </Col>
                     </Form.Group>
-                        <Col sm="8">
+                        <Col sm="12">
                              <Button disabled={!isEnabled} bsStyle="primary" type="submit">Save</Button>
                         </Col>
                     </Form>
