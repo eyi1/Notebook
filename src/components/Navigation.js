db8bc8e53ea717cc2e63c54ca296de1a890ec872
github username <github email address>
2019-02-27 20:17:29 -0500

    on click notebook brand will route to the home component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ export default class Navigation extends React.Component {
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#"><strong>noteböök</strong></Navbar.Brand>
+                <Navbar.Brand href="/"><strong>noteböök</strong></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
