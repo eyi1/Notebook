901ab22f96eedb0254ab56c0b41538c3983751ba
github username <github email address>
2019-03-01 16:37:22 -0500

    delete commented out line

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,6 @@ import Note from './Note'
 class Notes extends React.Component {     
     render(){
         const renderNotes = this.props.notesCollection.map(note => <Note key={note.id} note={note} deleteNote={this.props.deleteNote} toggleEdit={this.props.toggleEdit}/>)
-        //this.props.notebooksList.map(notebook => <Notebook key={notebook.id} notebook={notebook} />)
         
         return(
             <div>
