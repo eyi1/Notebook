fa8cb76a8614524acc8d67e07de76bc59eefde2b
github username <github email address>
2019-03-02 13:30:43 -0500

    made and delete changes made

--- src/components/Notebooks.js
+++ src/components/Notebooks.js
@@ -3,19 +3,20 @@ import Notebook from './Notebook'
 //import Table from 'react-bootstrap/Table'
 import {Container, Row, Col }from 'react-bootstrap'
 
-class Notebooks extends React.Component {     
+class Notebooks extends React.Component {    
+
     render(){
         const renderNotebooks = this.props.notebooksList.map(notebook => <Notebook key={notebook.id} notebook={notebook} deleteNotebook={this.props.deleteNotebook}/>)
         //this.props.notebooksList.map(notebook => <Notebook key={notebook.id} notebook={notebook} />)
-        
+
         return(
             <div className="table">
                 <Container>
                     <Row className="tableLabels">
                         <Col md="2"><strong>ID</strong></Col>
                         <Col md="4"><strong>Name</strong></Col>
-                        <Col md="2"><strong>Created By</strong></Col>
                         <Col md="2"><strong>Posted</strong></Col>
+                        <Col md="2"><strong>Created By</strong></Col>
                         <Col md="2"><strong>Actions</strong></Col>
                     </Row>
                 </Container>
@@ -24,6 +25,5 @@ class Notebooks extends React.Component {
         )
     }
 }
-
 
 export default Notebooks
