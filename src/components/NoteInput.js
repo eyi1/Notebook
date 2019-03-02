5e6778134021681ef1cad75faf70d3bfafa30252
github username <github email address>
2019-03-02 13:30:08 -0500

    add more rows in content

--- src/components/NoteInput.js
+++ src/components/NoteInput.js
@@ -36,7 +36,6 @@ class NoteInput extends React.Component{
             <div>
                 <Form onSubmit={this.handleOnSubmit}>
                     <Form.Group controlId="exampleForm.ControlInput1">
-                        {/* <Form.Label>Title</Form.Label> */}
                         <Col sm="12" >
                             <Form.Control size="lg" type="text" name='title' onChange={this.handleOnChange} value={this.state.title} placeholder="Title" />
                         </Col>
@@ -44,7 +43,7 @@ class NoteInput extends React.Component{
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         {/* <Form.Label>Write Something Here</Form.Label> */}
                         <Col sm="12">
-                            <Form.Control as="textarea" name='content' onChange={this.handleOnChange} value={this.state.content} rows="10" placeholder="Start writing"/>
+                            <Form.Control as="textarea" name='content' onChange={this.handleOnChange} value={this.state.content} rows="15" placeholder="Start writing"/>
                         </Col>
                     </Form.Group>
                         <Col sm="12">
