69e543f49e85ff86968beed908025f4ab0c41f87
github username <github email address>
2019-06-11 20:50:05 -0400

    import currentUserReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import notebooksReducer from './notebooksReducer'
 import notesReducer from './notesReducer'
+import currentUserReducer from './currentUserReducer'
 
 
 const rootReducer = combineReducers({ 
-notebooks: notebooksReducer,
-notes: notesReducer
+    notebooks: notebooksReducer,
+    notes: notesReducer,
+    currentUser: currentUserReducer
 })
 
 export default rootReducer;
