f6544e964f182da71f2faeab70002c68a4522151
github username <github email address>
2019-06-12 22:00:49 -0400

    wrap app inside router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -14,7 +14,9 @@ const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__
 
 ReactDOM.render(
 <Provider store={store}>
-    <App />
+    <Router>
+        <App />
+    </Router>
 </Provider>, 
 document.getElementById('root'));
 
