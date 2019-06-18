7a034944d2affe5b898c3b8afd22eb98cc538beb
github username <ej7729@gmail.com>
2019-06-18 05:36:50 -0400

    delete commented code

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,37 +1,10 @@
-import * as types from './actionTypes'
-
 const authLink = `/api/v1`
-
-//this function has an authentication request action type
-// const authRequest = () => {
-//     return {
-//       type: types.AUTHENTICATION_REQUEST
-//     }
-//   }
-  
-  //this function has an authentication success action type. When there's a success in correct credentials, the server passes a user and token.
-  // const authSuccess = (user, token) => {
-  //   return {
-  //     type: types.AUTHENTICATION_SUCCESS,
-  //     user: user,
-  //     token: token
-  //   }
-  // }
-  //this function has an authentication failure action type. When there are incorrect credentials, the server passes errors.
-  // const authFailure = (errors) => {
-  //   return {
-  //     type: types.AUTHENTICATION_FAILURE,
-  //     errors: errors
-  //   }
-  // }
-
-  //
 
   export const signup = (user) => {
     const newUser = user
     console.log(newUser)
     return dispatch => {
-      return fetch(`${authLink}/users`, {
+      return fetch(`${authLink}/signup`, {
         method: "POST",
         headers: {
           "Accept":"application/json",
@@ -45,8 +18,7 @@ const authLink = `/api/v1`
           //debugger;
           dispatch({ //dispatch action
             type: "SET_CURRENT_USER",
-            currentUser: jresp.user
-          
+            currentUser: jresp.user         
           })
           localStorage.setItem("token", jresp.jwt)
         })
@@ -58,50 +30,30 @@ const authLink = `/api/v1`
 
   export const login = (user) => {
     console.log(user)
-    debugger
+    //debugger
     return dispatch => {
       return fetch(`${authLink}/login`, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Accept":"application/json",
+          "Content-Type":"application/json"
         },
         body: JSON.stringify({user: user})
       })
         .then(response => response.json())
-       // .then(res => console.log(res))
-        .then(jresp => {
-          //debugger;
-          dispatch({ //dispatch action
-            type: "SET_CURRENT_USER",
-            currentUser: jresp.user          
-          })
-          localStorage.setItem("token", jresp.jwt)
-        })
-         .catch((errors) => {
+        .then(res => console.log(res))
+        // .then(jresp => {
+        //   dispatch({ //dispatch action
+        //     type: "SET_CURRENT_USER",
+        //     currentUser: jresp.user         
+        //   })
+        //   localStorage.setItem("token", jresp.jwt)
+        // })
+        .catch((errors) => {
             console.log(errors)
         })
-     }
+    };
   }
-
-  //export const login = (credentials) => {
-   //console.log(credentials)
-    // const request = new Request(`${authLink}/login`, {
-    //   method: "POST",
-    //   headers: new Headers({
-    //     "Content-Type": "application/json",
-    //     "Authorization": `Bearer ${localStorage.token}`,
-    //   }),
-    //   body: JSON.stringify({user: credentials})
-    // })
-    // return fetch(request)
-    //   .then(response => response.json())
-    //   .then(userJson => {console.log(userJson)})
-    //   //.then(userJson => {return userJson})
-    //   .catch(error => {
-    //     //return error;
-    //      console.log(error)
-    //   });
-  //}
 
   export const logout = () => {
     return dispatch => {
