a7af19dd8d1e8d31e04bf047570b56dea47b824d
github username <ej7729@gmail.com>
2019-06-18 05:45:12 -0400

    delete commented code

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -4,16 +4,8 @@ export default (state=null, action) => {
     switch(action.type){
         
         case "SET_CURRENT_USER":
-            //console.log('action.user', action.user)
+            //console.log('action')
             return action.currentUser
-        
-        // case types.ADD_NOTE:
-        //     console.log('action.add-note', action.note)
-        //     return [...state, action.note]
-
-        // case types.UPDATE_NOTE:
-        // console.log(action.note)
-        //     return state.map(note => note.id === action.note.id ? action.note : note)
         
         case "DELETE_USER":
             return {}
